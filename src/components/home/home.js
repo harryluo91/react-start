807edcb2de1a12cf4c98dc0826a0643ecd904f63
Harry Luo <hluo@konradgroup.com>
2018-04-13 11:19:34 -0400

    update

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,20 +4,21 @@ import { Link } from "react-router-dom";
 class Home extends Component {
   render() {
     return (
-      <div className="home">
-        <header className="home-header">
-            <h1 className="App-title">Welcome to React</h1>
-            <nav className="navbar navbar-light">
-                <ul className="nav navbar-nav">
-                    <li><Link to="/">Homes</Link></li>
-                    <li><Link to="/not-home">Not Home</Link></li>
-                </ul>
-            </nav>
-        </header>
-        <p className="home-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    //   <div className="home">
+    //     <header className="home-header">
+    //         <h1 className="App-title">Welcome to React</h1>
+    //         <nav className="navbar navbar-light">
+    //             <ul className="nav navbar-nav">
+    //                 <li><Link to="/">Homes</Link></li>
+    //                 <li><Link to="/not-home">Not Home</Link></li>
+    //             </ul>
+    //         </nav>
+    //     </header>
+    //     <p className="home-intro">
+    //       To get started, edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //   </div>
+    <div>Hello Su Yi Sheng~</div>
     );
   }
 }
